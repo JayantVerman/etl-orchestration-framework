35b24db17942ab42d78b566087bc5dd1976e80c1
JayantVerman <speedpost029@gmail.com>
2023-07-20 12:02:00 +0000

    feat(builtins): add flaky helper

--- src/etl_orchestrator/builtins.py
+++ src/etl_orchestrator/builtins.py
@@ -1,3 +1,5 @@
+import random
 from etl_orchestrator.engine import TaskContext
-def echo(ctx: TaskContext) -> None:
-    print(f'[{ctx.task_id}] running')
+def flaky(ctx: TaskContext):
+    if random.random() < 0.3:
+        raise RuntimeError('flaky')
